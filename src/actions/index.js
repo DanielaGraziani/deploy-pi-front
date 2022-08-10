c5ae103d13bd714494eba8c679f0ddba55524785
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-10 04:24:00 -0300

    change landing styles

--- src/actions/index.js
+++ src/actions/index.js
@@ -145,3 +145,4 @@ export const reset=()=>{
     dispatch({type: RESET})
   }
 }
+
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import s from '../styles/Landing.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import s from "../styles/Landing.module.css";
 
 export default function LandingPage() {
   return (
     <div className={s.landingContainer}>
       <div className={s.textContainer}>
-        <h2 className={s.landingTitle}>Welcome to</h2>
+        <h3 className={s.landingTitle}>
+          Welcome <span className={s.to}>to</span>
+        </h3>
         <div className={s.text}>
-            <h1>Recipe Ideas!</h1>
+          <h2>Recipe Ideas!</h2>
         </div>
       </div>
 
-    <div className={s.buttonContainer}>
-    <Link to= '/recipes'>
-    <button className={s.button}>Enter</button>
-    </Link>
+      <div className={s.buttonContainer}>
+        <Link to="/recipes">
+          <button className={s.button}>Enter</button>
+        </Link>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
